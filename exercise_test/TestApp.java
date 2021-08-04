cf857235f0c091f381714085021fe1f96062f006
Phạm Việt Long <phamvietlong.pass@gmail.com>
2021-08-04 22:58:45 +0700

    update

--- exercise_test/TestApp.java
+++ exercise_test/TestApp.java
@@ -13,10 +13,10 @@ public class TestApp {
 		
 		GeneralHandling gh = new GeneralHandling(person);
 			
-		Thread1 thread1 = new Thread1(gh);
-		Thread2 thread2 = new Thread2(gh);
-		Thread3 thread3 = new Thread3(gh);
-		Thread4 thread4 = new Thread4(gh);
+		Thread1 thread1 = new Thread1(gh); // CHECK FILE Mom.dat
+		Thread2 thread2 = new Thread2(gh); // CHECK FILE Dad.dat
+		Thread3 thread3 = new Thread3(gh); // CHECK FILE UBND.dat
+		Thread4 thread4 = new Thread4(gh); // WRITE IN TO FILE result.bin
 		
 		thread1.start();
 		thread2.start();	
