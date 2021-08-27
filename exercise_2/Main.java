2e6ce6ee55122729500f8196de5fd22c15db6ebe
Phạm Việt Long <phamvietlong.pass@gmail.com>
2021-08-27 18:19:55 +0700

    update

--- exercise_2/Main.java
+++ exercise_2/Main.java
@@ -1,5 +1,11 @@
 package exercise_2;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+
 import exercise_2.reading.ReadingConfigFile;
 import exercise_2.reading.ReadingInputFile;
 import exercise_2.writing.WritingInputFile;
@@ -9,6 +15,7 @@ public class Main {
 	public static void main(String[] args) {		
 		ReadingConfigFile config = new ReadingConfigFile();
 		int Max_Thread = config.getMax_Thread();
+		int Size_Array = config.getSize();
 		
 //		WritingInputFile wif = new WritingInputFile();
 //		wif.writeToFileInput("ex_2_input.txt");
@@ -16,14 +23,38 @@ public class Main {
 		ReadingInputFile rif = new ReadingInputFile();
 		
 		Integer[] arr = rif.readFile("ex_2_input.txt");
-		System.out.println("Length : " + arr.length);
-
 		Sort sort = new Sort();
 		long startTime = System.currentTimeMillis();
 		sort.threadSort(arr, Max_Thread);
 		long endTime = System.currentTimeMillis();
 		
 		System.out.println("Time : " + (endTime-startTime));
+		
+		// Benchmark
+		FileWriter fw = null;
+		BufferedWriter bw = null;
+		try {
+			File file = new File("Benchmark.txt");
+			if(!file.exists()) {
+				file.createNewFile();
+			}
+			fw = new FileWriter(file, true);
+			bw = new BufferedWriter(fw);
+			String s = Max_Thread + " Thread - " + Size_Array + 
+					" phần tử ==> " + (endTime-startTime) + "ms" + "\n";
+			bw.write(s);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(bw != null) {
+					bw.close();
+				}
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		
 		
 	}
 
